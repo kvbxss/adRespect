b55321d2e87545f3f3f995daf521e8610c3fa9f4
kvbxss <kubadownar444@gmail.com>
2023-08-21 23:56:55 +0200

    dropdown and searchbar script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-const masonry = new Macy({
+var macyInstance = Macy({
   container: ".masonry",
+  mobileFirst: true,
   trueOrder: false,
   waitForImages: false,
-  margin: 24,
+  margin: 43,
   columns: 3,
   breakAt: {
     1200: 3,
@@ -10,3 +11,50 @@ const masonry = new Macy({
     520: 1,
   },
 });
+
+var menu = document.getElementById("menu");
+var dropdownimg = document.getElementById("arrow-dropdown");
+var search = document.getElementById("search");
+var searchimg = document.getElementById("magn-glass");
+function toggleMenu() {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+}
+
+function toggleSearch() {
+  if (search.classList.contains("hidden")) {
+    search.classList.remove("hidden");
+  } else {
+    search.classList.add("hidden");
+  }
+}
+
+window.onclick = function (event) {
+  dropdownimg.style.transform = "rotate(180deg)";
+  if (menu.classList.contains("hidden")) {
+    dropdownimg.style.transform = "rotate(0deg)";
+  }
+
+  var dropdownWrapper = document.getElementById("dropdown-wrapper");
+  if (
+    !dropdownWrapper.contains(event.target) &&
+    !menu.classList.contains("hidden")
+  ) {
+    menu.classList.add("hidden");
+  }
+
+  searchimg.style.transform = "rotate(90deg)";
+  if (search.classList.contains("hidden")) {
+    searchimg.style.transform = "rotate(0deg)";
+  }
+  var searchWrapper = document.getElementById("search-wrapper");
+  if (
+    !searchWrapper.contains(event.target) &&
+    !search.classList.contains("hidden")
+  ) {
+    search.classList.add("hidden");
+  }
+};
